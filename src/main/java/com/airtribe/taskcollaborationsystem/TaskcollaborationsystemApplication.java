cc0b29153719666740ef49f6820d55af6e9a88e1
Ruchir-BridgeLabz <ruchir.dixit@bridgelabz.com>
2025-01-25 15:56:22 +0530

    Project setup

---
+++ src/main/java/com/airtribe/taskcollaborationsystem/TaskcollaborationsystemApplication.java
@@ -0,0 +1,13 @@
+package com.airtribe.taskcollaborationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskcollaborationsystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskcollaborationsystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/airtribe/taskcollaborationsystem/TaskcollaborationsystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airtribe.taskcollaborationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskcollaborationsystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
